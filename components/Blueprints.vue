b08650ec9cc81c65b3f4430fd35ccd66694db14d
towerhufham <32599109+towerhufham@users.noreply.github.com>
2022-04-24 23:02:42 -0400

    better build button (b/b/b)

--- components/Blueprints.vue
+++ components/Blueprints.vue
@@ -7,30 +7,35 @@ export default {
     const store = useStore()
     const state = reactive({isBuilding: false});
 
-    function toggleIsBuilding() {
-      state.isBuilding = !state.isBuilding
-    }
-
-    function buildingButtontext() {
-      return state.isBuilding ? "Building..." : "Build🔨"
+    function tryStartBuilding() {
+      if (!state.isBuilding) {
+        if (store.inventory.Builds > 0) {
+          state.isBuilding = true;
+        } else {
+          alert("You need a 🔨 to build.")
+        }
+      } else {
+        state.isBuilding = false;
+      }
     }
 
     function handleBuild(as: ActionSpace) {
       if (state.isBuilding) {
         store.buildAction(as);
         state.isBuilding = false;
+        store.inventory.Builds--;
       }
     }
 
-    return { store, buildingButtontext, toggleIsBuilding, handleBuild }
+    return { store, state, tryStartBuilding, handleBuild }
   }
 }
 </script>
 
 <template>
   <div class="flex gap-5 p-3 bg-blue-300">
-    <button @click="toggleIsBuilding" class="border-2 border-red-500 bg-red-100 rounded-lg shadow-md p-1 hover:bg-red-50 text-3xl">
-      {{ buildingButtontext() }}
+    <button @click="tryStartBuilding" class="border-2 border-red-500 bg-red-100 rounded-lg shadow-md p-1 hover:bg-red-50 text-3xl">
+      {{ state.isBuilding ? "Building..." : "Build🔨" }}
     </button>
     <ActionSpace v-for="action in store.blueprintSpaces" v-bind:action="action" :key="action.name" @click.native="handleBuild(action)" />
   </div>
